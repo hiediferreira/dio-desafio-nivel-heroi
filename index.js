6cecf9ac4abe59901ed3b6346330a361177773d7
Hiedi Ferreira <hiedyferreira@gmail.com>
2024-10-24 16:07:08 -0300

    :sparkles: feat: Entregando o desafio

--- index.js
+++ index.js
@@ -15,3 +15,30 @@
     Ao final deve se exibir uma mensagem:
     "O Herói de nome **{nome}** está no nível de **{nivel}**"
 */
+
+var nome = 'Hiédi'
+var nivel = ''
+
+function classificaHeroi(XP){
+    if(XP <= 1000){
+        nivel = 'Ferro'
+    } else if(XP > 1000 && XP <= 2000){
+        nivel = 'Bronze'
+    } else if(XP > 2000 && XP <= 5000){
+        nivel = 'Prata'
+    } else if(XP > 5000 && XP <= 7000){
+        nivel = 'Ouro'
+    } else if(XP > 7000 && XP <= 8000){
+        nivel = 'Platina'
+    } else if(XP > 8000 && XP <= 9000){
+        nivel = 'Ascendente'
+    } else if(XP > 9000 && XP <= 10000){
+        nivel = 'Imortal'
+    } else {
+        nivel = 'Radiante'
+    }
+
+    return `O Herói de nome ${nome} está no nível de ${nivel}.`
+}
+
+console.log(classificaHeroi(10001))
